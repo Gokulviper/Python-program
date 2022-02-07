fa3a1a41427e28b3d5f5ccdd7b824aaec0b9e04c
Gokulviper <deangokul57@gmail.com>
2022-02-07 11:57:47 +0530

    first commit

---
+++ pythonProject/venv/sampel.py
@@ -0,0 +1,46 @@
+s1='gokul'
+s2='rajanarajn'
+ans='';
+templen=0
+length=0
+current='';
+if len(s1)<len(s2):
+    length=len(s1)
+    current=s2
+    templen=len(s2)
+else:
+    length=len(s2)
+    current=s1
+    templen = len(s1)
+i=0
+for i in range(0,length):
+    ans=ans+s1[i]+s2[i]
+for i in range(0,templen):
+    ans=ans+current[i]
+print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

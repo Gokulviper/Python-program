19389721cae0d73bc63917e1097494db58b7a4f2
Gokulviper <deangokul57@gmail.com>
2022-02-09 11:55:16 +0530

    updated

---
+++ pythonProject/venv/CompoundDataTypes/List/List.py
@@ -0,0 +1,24 @@
+#converting String to list
+s='happy new year'
+s=s.split(" ")
+print(s)
+#list FUunvtions
+list=[10,30,10,45,15,10]
+count=0
+num=10
+#for i in list:
+print(list.count(10))
+print(list.pop())
+print(list.pop())
+print(list.sort())
+print(list)
+print(list.append(34434))
+print(list)
+print(list.insert(0,23))
+print(list)
+print(list.reverse())
+print(list)
+
+list.remove(45)
+print(list)
+
---
+++ pythonProject/venv/CompoundDataTypes/List/Reverse program.py
@@ -0,0 +1,5 @@
+li=[12,34,546,67,787]
+ri=[]
+for i  in li:
+    ri.insert(0,i)
+print(ri)

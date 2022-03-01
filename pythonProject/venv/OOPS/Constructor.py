5e601d857dc8e75e29541c810d8267d6b5f4b7d6
Gokulviper <deangokul57@gmail.com>
2022-03-01 14:58:39 +0530

    updated

---
+++ pythonProject/venv/OOPS/Constructor.py
@@ -0,0 +1,14 @@
+class superMa:
+    def __init__(self,brand,price,discount):
+        self.brand=brand
+        self.price=546
+        self.discount=discount
+
+    def __init__(self, brand, discount):
+        self.brand = brand
+        self.price = 546
+        self.discount = discount
+
+bread=superMa('dfg',234)
+print(type(bread.brand))
+print(bread.price)
